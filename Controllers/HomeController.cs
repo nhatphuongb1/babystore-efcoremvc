0cd5e5a7ba0c28e8a9b60cdf6584036682ddb030
Jeremy <jeremy@enclave.vn>
2019-09-30 11:15:04 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BabyStore.Models;
+
+namespace BabyStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is BabyStore!!!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewBag.PhoneNumber = "0392502010";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
